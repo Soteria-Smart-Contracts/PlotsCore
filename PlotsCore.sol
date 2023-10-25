95d0f290c9b77772dd075b61875cd359d2717bb0
Darius Tanz <scratchticketseb@gmail.com>
2023-10-25 14:03:56 +0000

    Oct 25, 2023, 10:03 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.19;
 
 contract PlotsCore{
-    // Mapping that handles all the av
+
+    // Mapping that handles all the available tokens for borrowing for a collection
     mapping(address => uint256[]) public AvailableTokensByCollection;
+    
 }

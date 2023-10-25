033e0250db35bb587dcb30f4e6ca934610d34de9
Darius Tanz <scratchticketseb@gmail.com>
2023-10-25 14:37:28 +0000

    Oct 25, 2023, 10:37 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 contract PlotsCore{
     //Variable and pointer Declarations
-    address public PlotsTreasury;
+    address public Treasury;
     address[] public ListedCollections;
 
 

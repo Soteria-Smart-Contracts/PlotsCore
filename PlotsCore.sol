c7f9e13e0ecacdc9ab0ed7f38574e8ef01b73684
Darius Tanz <scratchticketseb@gmail.com>
2023-10-25 14:38:10 +0000

    Oct 25, 2023, 10:38 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -21,7 +21,7 @@ contract PlotsCore{
     
 
     constructor(address [] memory _admins){
-        Treasury = address(new PlotsTreasury());
+        Treasury = address(new PlotsTreasury(address(this)));
         for(uint256 i = 0; i < _admins.length; i++){
             Admins[_admins[i]] = true;
         }

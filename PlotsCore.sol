84a5875c3192130cac82093ad283ba47a34af2ee
Darius Tanz <scratchticketseb@gmail.com>
2023-10-29 22:43:58 +0000

    Oct 29, 2023, 6:43 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -77,22 +77,7 @@ contract PlotsTreasury{
 
 
 interface ERC721 /* is ERC165 */ {
-    event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
-    event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
-    event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
-
-    /// @notice Count all NFTs assigned to an owner
-    /// @dev NFTs assigned to the zero address are considered invalid, and this
-    ///  function throws for queries about the zero address.
-    /// @param _owner An address for whom to query the balance
-    /// @return The number of NFTs owned by `_owner`, possibly zero
     function balanceOf(address _owner) external view returns (uint256);
-
-    /// @notice Find the owner of an NFT
-    /// @dev NFTs assigned to zero address are considered invalid, and queries
-    ///  about them do throw.
-    /// @param _tokenId The identifier for an NFT
-    /// @return The address of the owner of the NFT
     function ownerOf(uint256 _tokenId) external view returns (address);
 
     /// @notice Transfers the ownership of an NFT from one address to another address

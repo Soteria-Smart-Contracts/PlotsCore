0b6f55188d6974681f26069da779f8fdf00b83b2
Darius Tanz <scratchticketseb@gmail.com>
2023-10-29 22:43:37 +0000

    Oct 29, 2023, 6:43 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -75,15 +75,8 @@ contract PlotsTreasury{
 }
 
 
-/// @title ERC-721 Non-Fungible Token Standard
-/// @dev See https://eips.ethereum.org/EIPS/eip-721
-///  Note: the ERC-165 identifier for this interface is 0x80ac58cd.
+
 interface ERC721 /* is ERC165 */ {
-    /// @dev This emits when ownership of any NFT changes by any mechanism.
-    ///  This event emits when NFTs are created (`from` == 0) and destroyed
-    ///  (`to` == 0). Exception: during contract creation, any number of NFTs
-    ///  may be created and assigned without emitting Transfer. At the time of
-    ///  any transfer, the approved address for that NFT (if any) is reset to none.
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
 
     /// @dev This emits when the approved address for an NFT is changed or

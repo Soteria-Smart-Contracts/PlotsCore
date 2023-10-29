7f139f8a406fdbe09000860ff4b9dc51030e14c2
Darius Tanz <scratchticketseb@gmail.com>
2023-10-29 22:36:55 +0000

    Oct 29, 2023, 6:36 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -27,6 +27,12 @@ contract PlotsCore{
         }
     }
 
+    //Public Functions
+
+    function GetAvailableTokensByCollection(address _collection) public view returns(uint256[] memory){
+        return AvailableTokensByCollection[_collection];
+    }
+
 
     //Only Admin Functions
 

8f3ac41b5c8a78f61e946fbc181bead8be662bcb
Darius Tanz <scratchticketseb@gmail.com>
2023-10-29 22:44:43 +0000

    Oct 29, 2023, 6:44 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -83,17 +83,7 @@ interface ERC721 /* is ERC165 */ {
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
     function approve(address _approved, uint256 _tokenId) external payable;
     function setApprovalForAll(address _operator, bool _approved) external;
-
-    /// @notice Get the approved address for a single NFT
-    /// @dev Throws if `_tokenId` is not a valid NFT.
-    /// @param _tokenId The NFT to find the approved address for
-    /// @return The approved address for this NFT, or the zero address if there is none
     function getApproved(uint256 _tokenId) external view returns (address);
-
-    /// @notice Query if an address is an authorized operator for another address
-    /// @param _owner The address that owns the NFTs
-    /// @param _operator The address that acts on behalf of the owner
-    /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
     function isApprovedForAll(address _owner, address _operator) external view returns (bool);
 }
 

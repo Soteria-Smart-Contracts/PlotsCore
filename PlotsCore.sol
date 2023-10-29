625bb8c05913bee8e2154e762d4e953b934763af
Darius Tanz <scratchticketseb@gmail.com>
2023-10-29 22:41:38 +0000

    Oct 29, 2023, 6:41 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -45,6 +45,7 @@ contract PlotsCore{
         require(ListedCollectionsIndex[Collection] != 0, "Collection not listed");
         require(AvailableTokensByCollectionIndex[Collection][TokenId] == 0, "Token already listed");
         //require that the treasury or lend contract has the token
+
         
 
         AvailableTokensByCollection[Collection].push(TokenId);

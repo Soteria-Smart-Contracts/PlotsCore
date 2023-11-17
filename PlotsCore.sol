be4e8d5f262354bf05373c6adfd1b7a4cfeab170
Darius Tanz <scratchticketseb@gmail.com>
2023-11-17 21:00:44 +0000

    Nov 17, 2023, 4:00 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -43,7 +43,7 @@ contract PlotsCore{
     mapping(address => uint256[]) public AvailableTokensByCollection;
     mapping(address => mapping(uint256 => uint256)) public AvailableTokensByCollectionIndex;
 
-    
+    //
 
     mapping(address => uint256) public ListedCollectionsIndex;
     mapping(address => mapping(address => uint256)) public OwnershipByPurchase;

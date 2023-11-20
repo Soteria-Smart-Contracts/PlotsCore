75b275a125209f4293a0f2b03b11ff2b97bbc0cb
Darius Tanz <scratchticketseb@gmail.com>
2023-11-20 13:56:47 +0000

    Nov 20, 2023, 8:56 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -76,7 +76,7 @@ contract PlotsCore {
 
     //Public Functions
 
-    function RequestToken(address Collection, uint256 TokenId, LengthOption) public payable {
+    function RequestToken(address Collection, uint256 TokenId, LengthOption Duration) public payable {
         require(ListedCollectionsIndex[Collection] != 0, "Collection not listed");
         require(ListingByCollectionIndex[Collection][TokenId] != 0, "Token not listed");
         require(OwnershipByPurchase[Collection][msg.sender] == 0, "Already requested token");

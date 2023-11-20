b3279f13b56b4e350552ece2e7d1becaec8ad296
Darius Tanz <scratchticketseb@gmail.com>
2023-11-20 17:19:48 +0000

    Nov 20, 2023, 12:19 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -136,7 +136,12 @@ contract PlotsCore {
     }
 
     function GetListedCollectionWithPrices(address _collection, uint256 _tokenId) public view returns(Listing memory, uint256[] memory Prices){
-        
+        Listing memory _listing = ListingByCollection[_collection][_tokenId];
+        uint256[] memory _prices = new uint256[](LoansByCollection[_collection].length);
+        for(uint256 i = 0; i < LoansByCollection[_collection].length; i++){
+            _prices[i] = LoansByCollection[_collection][i].LoanLength;
+        }
+        return (_listing, _prices);
     }
 
     //Internal Functions

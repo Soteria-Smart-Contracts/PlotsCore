463f519b217072f16b4f658d93660fb34926c4c5
Darius Tanz <scratchticketseb@gmail.com>
2023-11-22 15:32:58 +0000

    Nov 22, 2023, 10:32 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -251,7 +251,7 @@ contract PlotsTreasury{
     }
 
     function SendToLoan(address Collection, uint256 TokenID, PlotsCore.OwnershipPercent Ownership) external OnlyCore{
-
+        
 
         TokenInLoan[Collection][TokenID] = true;
     }

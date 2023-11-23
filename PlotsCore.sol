0e7f7e0742984374ac347b168283f307548a1d87
Darius Tanz <scratchticketseb@gmail.com>
2023-11-23 17:45:06 +0000

    Nov 23, 2023, 12:45 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -259,7 +259,7 @@ contract PlotsTreasury{
     //return from loan (transferfrom the token location back to the treeasury, set token location to this)
     function ReturnFromLoan(address Collection, uint256 TokenID) external {
         //require that the token is this contract
-        ERC721
+        ERC721()
 
         TokenLocation[Collection][TokenID] = address(this);
     }

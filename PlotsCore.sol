6938a579f3f5479084c2c5e33e6592b54e503a3c
Darius Tanz <scratchticketseb@gmail.com>
2023-11-23 22:16:27 +0000

    Nov 23, 2023, 5:16 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -84,7 +84,7 @@ contract PlotsCore {
                 BorrowCost += (TokenValue * 25) / 100;
             }
             require(msg.value >= BorrowCost, "Not enough ether sent");
-            require(PlotsTreasury(Treasury).SendToLoan(NewLoanContract, Collection, TokenId), "Token not sent to loan contract");
+            PlotsTreasury(Treasury).SendToLoan(NewLoanContract, Collection, TokenId);
         }
         else{
             require(msg.value == 0, "Do not Pay for usage tokens");

e83633c0388fadfdecd39c7d43002b9433a0252e
Darius Tanz <scratchticketseb@gmail.com>
2023-11-23 22:25:35 +0000

    Nov 23, 2023, 5:25 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -90,6 +90,7 @@ contract PlotsCore {
             require(msg.value == 0, "Do not Pay for usage tokens");
         }
 
+        RemoveListingFromCollection();
         NFTLoan(NewLoanContract).BeginLoan(Ownership, ListingsByCollection[Collection][TokenId].Lister , msg.sender, Collection, TokenId, DurationUnix, TokenValue);
         OwnershipByPurchase[Collection][msg.sender] = TokenId;
     }

eb28095d4d04637323804c3c7d003c9b85682abe
Darius Tanz <scratchticketseb@gmail.com>
2023-11-23 22:01:40 +0000

    Nov 23, 2023, 5:01 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -331,6 +331,7 @@ contract NFTLoan{
 
     function BeginLoan(PlotsCore.OwnershipPercent Ownership, address TokenOwner, address TokenBorrower, address Collection, uint256 TokenId, uint256 Duration, uint256 InitialValue) public OnlyManager {
         require(msg.sender == Manager, "Only Loans Or Treasury Contract can interact with this contract");
+        //require that the token is this contract by calling the collection and token id in the token location mapping
 
         TokenCollection = Collection;
         TokenID = TokenId;

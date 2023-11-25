165610eda4518822c4d03dcce924cf1dac9dd5d8
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 17:11:10 +0000

    Nov 25, 2023, 12:11 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -108,7 +108,8 @@ contract PlotsCoreV1 {
         uint256 TokenId = NFTLoan(LoanContract).TokenID();
 
         NFTLoan(LoanContract).EndLoan(FeeReceiver);
-            }
+
+    }
 
     // Listings ---------------------------------------------------------------------------------
 

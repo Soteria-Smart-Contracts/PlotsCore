a34dcd7180122289d347c3acb36dbb987831e58c
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 18:13:00 +0000

    Nov 25, 2023, 1:13 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -112,9 +112,11 @@ contract PlotsCoreV1 {
         address Collection = NFTLoan(LoanContract).TokenCollection();
         uint256 TokenId = NFTLoan(LoanContract).TokenID();
         uint256 OwnershipPercentage;
+        address ReturnCon
 
         if(NFTLoan(LoanContract).OwnershipType() == OwnershipPercent.Zero){
             OwnershipPercentage = 0;
+
         }
         else if(NFTLoan(LoanContract).OwnershipType() == OwnershipPercent.Ten){
             OwnershipPercentage = 10;

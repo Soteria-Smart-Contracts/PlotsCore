40b13ab215374c1b28ab43d0c24c1bfeb40b46c2
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 20:30:28 +0000

    Nov 25, 2023, 3:30 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -437,7 +437,7 @@ contract PlotsLend{
         TokenDepositor[Collection][TokenId] = msg.sender;
         TokenLocation[Collection][TokenId] = address(this);
         AllUserTokens[msg.sender].push(Token(Collection, TokenId));
-
+        AllUserTokensIndex[msg.sender][Collection][TokenId] = AllUserTokens[msg.sender].length - 1;
     }
 
     function WithdrawToken(address Collection, uint256 TokenId) public{

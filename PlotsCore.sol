7b8ab1577151c96af13f8057c5add729b49474bc
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 18:29:00 +0000

    Nov 25, 2023, 1:29 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -176,7 +176,7 @@ contract PlotsCoreV1 {
         require(CurrentOwnership != Ownership, "Ownership already set to this");
 
         uint256 CurrentValue = PlotsTreasury(Treasury).GetTokenValueFloorAdjusted(NFTLoan(LoanContract).TokenCollection(), NFTLoan(LoanContract).TokenID());
-
+        uint256 ValueChange;
         if(CurrentOwnership == OwnershipPercent.Ten){
             Chan = (Plo(LoanContract).InitialValue() * 10) / 100;
         }

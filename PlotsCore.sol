b511165357c5dd81d3d2ca104508a0656724e668
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 17:14:20 +0000

    Nov 25, 2023, 12:14 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -109,7 +109,6 @@ contract PlotsCoreV1 {
         uint256 InitialValue = NFTLoan(LoanContract).InitialValue();
         uint256 CurrentValue = PlotsTreasury(Treasury).GetTokenValueFloorAdjusted(Collection, TokenId);
 
-        
 
         NFTLoan(LoanContract).EndLoan(FeeReceiver);
         PlotsTreasury(Treasury).ReturnedFromLoan(Collection, TokenId);

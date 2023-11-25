1ac9f842eeff94674687f4742950a805dbb37d58
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 18:29:45 +0000

    Nov 25, 2023, 1:29 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -179,7 +179,7 @@ contract PlotsCoreV1 {
         uint256 CollateralValueChange;
 
         if(CurrentOwnership == OwnershipPercent.Ten){
-            
+            CollateralValueChange = (CurrentValue * 15) / 100;
         }
         else if(Ownership == OwnershipPercent.TwentyFive){
             ValueDifference = (NFTLoan(LoanContract).InitialValue() * 25) / 100;

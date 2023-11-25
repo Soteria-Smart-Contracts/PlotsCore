39c62cff5b1611c8346ec87e39659a11b22e1646
Darius Tanz <scratchticketseb@gmail.com>
2023-11-25 20:21:14 +0000

    Nov 25, 2023, 3:21 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -418,6 +418,7 @@ contract PlotsLend{
     }
 
     mapping(address => mapping(uint256 => address)) public TokenDepositor;
+    //token lo
 
     //allow a user to deposit a token into the lending contract from any collection that is listed on the core contract
     function DepositToken(address Collection, uint256 TokenId) public{

e7177fca41b517d650752581f16c22cb38469757
Darius Tanz <scratchticketseb@gmail.com>
2023-11-29 00:42:02 +0000

    Nov 28, 2023, 7:42 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -169,7 +169,7 @@ contract PlotsCoreV1 {
         if(Admins[msg.sender]){
             require(ERC721(Collection).ownerOf(TokenId) == Treasury, "Token not owned by treasury");
             require(ListingsByCollectionIndex[Collection][TokenId] == 0, "Token already listed");
-            //check
+            //check if token
             ListingsByCollection[Collection].push(Listing(Treasury, Collection, TokenId, ListingType.Ownership));
         }
         else{

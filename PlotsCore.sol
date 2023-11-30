d5949eb44a807819fef5f4ebe6d8f01e7352b67c
Darius Tanz <scratchticketseb@gmail.com>
2023-11-30 20:34:13 +0000

    Nov 30, 2023, 3:34 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -76,7 +76,7 @@ contract PlotsCoreV1 {
 
     function BorrowToken(address Collection, uint256 TokenId, LengthOption Duration, OwnershipPercent Ownership) public payable {
         require(ListedCollectionsIndex[Collection] != 0, "Collection not listed");
-        require(ListingsByCollection[ListingsByCollectionIndex[Collection][TokenId]].Lister !, "Token not listed");
+        require(ListingsByCollection[ListingsByCollectionIndex[Collection][TokenId]].Lister != address(0), "Token not listed");
 
         address LoanContract;
         if(AvailableLoanContracts.length > 0){

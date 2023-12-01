edfce132829483f6d19b6ca6cbea4153b485f6ee
Darius Tanz <scratchticketseb@gmail.com>
2023-12-01 13:35:10 +0000

    Dec 1, 2023, 8:35 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -8,6 +8,7 @@ contract PlotsCoreV1 {
     uint256 public CurrentRewardFee;
     address public LendContract;
     address[] public ListedCollections;
+    mapping(address => uint256) public ListedCollections;
     mapping(address => uint256) public ListedCollectionsIndex;
     mapping(OwnershipPercent => uint8) public OwnershipPercentages;
     address[] public AvailableLoanContracts;

52947147fd0c4fc6185ba954c9a2f8191c4700a8
Darius Tanz <scratchticketseb@gmail.com>
2023-12-01 21:02:22 +0000

    Dec 1, 2023, 4:02 PM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -48,6 +48,8 @@ contract PlotsCoreV1 {
 
     mapping(address => Listing[]) public ListingsByCollection;
     mapping(address => mapping(uint256 => uint256)) public ListingsByCollectionIndex;
+    //ListedBool is used to check if a token is listed, and if it is, what the index is in the ListingsByCollection array
+    //mapping(address => mapping(uint256 => bool)) public ListedBool;
 
     mapping(address => address[]) public AllUserLoans; //Outgoing loans
     mapping(address => mapping(address => uint256)) public AllUserLoansIndex;

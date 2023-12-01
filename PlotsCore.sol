57babe397cdfdfd0ddcf134f16d0a298e922a3a9
Darius Tanz <scratchticketseb@gmail.com>
2023-12-01 13:42:02 +0000

    Dec 1, 2023, 8:42 AM

--- PlotsCore.sol
+++ PlotsCore.sol
@@ -303,7 +303,7 @@ contract PlotsCoreV1 {
     function AddCollection(address _collection) public OnlyAdmin{
         ListedCollections.push(_collection);
         ListedCollectionsIndex[_collection] = ListedCollections.length - 1;
-        
+        ListedCollectionsMap
     }
 
     function RemoveCollection(address _collection) public OnlyAdmin{

051ff4ac90f39dd3ca23f756eef4216e2a2d2536
Darius Tanz <scratchticketseb@gmail.com>
2023-12-12 18:01:18 +0000

    Dec 12, 2023, 1:01 PM

--- Plots.sol
+++ Plots.sol
@@ -127,7 +127,7 @@ contract PlotsCoreV1 {
 
     function CloseLoan(address LoanContract) public{
         //allow admins to close loans early
-        require(NFTLoan(LoanContract).Borrower() == msg.sender || NFTLoan(LoanContract).Owner() == msg.sender || , "Not involved in loan");
+        require(NFTLoan(LoanContract).Borrower() == msg.sender || NFTLoan(LoanContract).Owner() == msg.sender || Admins[msg.sender], "Not involved in loan");
         require(NFTLoan(LoanContract).LoanEndTime() <= block.timestamp, "Loan not ended yet");
         require(NFTLoan(LoanContract).Active(), "Loan not active");
 

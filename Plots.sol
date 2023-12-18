04369b446f5f496bb40df929bd83efdd0bcc7260
Darius Tanz <scratchticketseb@gmail.com>
2023-12-18 22:29:32 +0000

    Dec 18, 2023, 5:29 PM

--- Plots.sol
+++ Plots.sol
@@ -421,7 +421,7 @@ contract PlotsTreasuryV1{
             require(PlotsCoreV1(PlotsCoreContract).ListedCollectionsMap(Collections[i]) == true, "Collection not listed");
             CollectionFloorPrice[Collections[i]] = FloorPrices[i];
 
-            //loop
+            //loop 
         }
     }
 

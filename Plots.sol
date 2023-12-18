f79b8cca750d8bbcc2210c3f39c8303c92ef440d
Darius Tanz <scratchticketseb@gmail.com>
2023-12-18 21:15:03 +0000

    Dec 18, 2023, 4:15 PM

--- Plots.sol
+++ Plots.sol
@@ -255,18 +255,9 @@ contract PlotsCoreV1 {
     }
 
 
-    function GetUserListingsByUser(address user) public view returns (Listing[] memory) {
-        Listing[] memory userListings;
-            for (uint256 i = 0; i < ListedCollections.length; i++) {
-                Listing[] memory collectionListings = ListingsByCollection[ListedCollections[i]];
-                for (uint256 j = 0; j < collectionListings.length; j++) {
-                    if (collectionListings[j].Lister == user) {
-                        userListings[userListings.length++] = collectionListings[j];
-                    }
-                }
-            }
-            return userListings;
-        }
+    function GetUserListingsByUser(address user) public view returns (Listing[] memory){
+        
+    }
 
 
 

236d20378e5d5d28aeb5ae5219cd0fd44191e220
Darius Tanz <scratchticketseb@gmail.com>
2023-12-18 22:17:44 +0000

    Dec 18, 2023, 5:17 PM

--- Plots.sol
+++ Plots.sol
@@ -216,6 +216,7 @@ contract PlotsCoreV1 {
             require(ERC721(Collection).ownerOf(TokenId) == LendContract, "Token not owned by lending contract");
             require(PlotsLendV1(LendContract).GetTokenDepositor(Collection, TokenId) == msg.sender, "Not owner of token");
             AddListingToCollection(Collection, TokenId, Listing(msg.sender, Collection, TokenId, ListingType.Usage));
+            //add 
         }
 
         ListedBool[Collection][TokenId] = true;

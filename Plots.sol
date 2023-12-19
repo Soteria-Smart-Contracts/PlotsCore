2f2ae240fe67120abcbc81b9bc7af8973505feaa
Darius Tanz <scratchticketseb@gmail.com>
2023-12-19 16:25:08 +0000

    Dec 19, 2023, 11:25 AM

--- Plots.sol
+++ Plots.sol
@@ -354,7 +354,7 @@ contract PlotsCoreV1 {
         delete ListedCollectionsMap[_collection];
     }
 
-    //function update payout tracker only callable by loan contracts (isloancontract mapping), input for a 
+    //function update payout tracker only callable by loan contracts (isloancontract mapping), input for a user and a token
 }
 
 contract PlotsTreasuryV1{

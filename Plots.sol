ddfc798e166ca81de330e981e46cd57bbf7edd47
Darius Tanz <scratchticketseb@gmail.com>
2023-12-19 20:57:01 +0000

    Dec 19, 2023, 3:57 PM

--- Plots.sol
+++ Plots.sol
@@ -17,7 +17,7 @@ contract PlotsCoreV1 {
     mapping(address => mapping(uint256 => address)) public LoanContractByToken;
     mapping(address => bool) public IsLoanContract;
     mapping(address => mapping(address => uint256)) RewardPayoutTracker;
-    
+    //map a list of users to
 
 
     enum ListingType{

2dac448fe7837bdd7f9e011c88139ff5244f01c5
Darius Tanz <scratchticketseb@gmail.com>
2023-12-19 16:17:23 +0000

    Dec 19, 2023, 11:17 AM

--- Plots.sol
+++ Plots.sol
@@ -15,7 +15,7 @@ contract PlotsCoreV1 {
     address[] public AvailableLoanContracts;
     mapping(address => uint256) public AvailableLoanContractsIndex;
     mapping(address => bool) public IsLoanContract;
-    mapping(address => mapping())
+    mapping(address => mapping(address => uint256))
 
 
     enum ListingType{

ab58fc4f7985174af07bf949fe972501a44af4f7
Darius Tanz <scratchticketseb@gmail.com>
2023-12-19 16:26:12 +0000

    Dec 19, 2023, 11:26 AM

--- Plots.sol
+++ Plots.sol
@@ -664,7 +664,7 @@ contract NFTLoan{
 
         uint256 OwnerReward = (RewardBalance * (10000 - BorrowerRewardShare)) / 10000;
 
-        
+        PlotsCoreV1(Manager).UpdatePayoutTracker(Owner, TokenCollection, OwnerReward);
         
         ERC20(RewardToken).transfer(Owner, OwnerReward);
         ERC20(RewardToken).transfer(Borrower, ERC20(RewardToken).balanceOf(address(this)));

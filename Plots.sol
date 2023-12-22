0284f8fe6691b3c91ac0db2b45acc6fa908ad3ac
Darius Tanz <scratchticketseb@gmail.com>
2023-12-22 15:46:23 +0000

    Dec 22, 2023, 10:46 AM

--- Plots.sol
+++ Plots.sol
@@ -73,6 +73,7 @@ contract PlotsCoreV1 {
             Admins[_admins[i]] = true;
         }
         Admins[msg.sender] = true;
+        Admins[Treasury]
 
         OwnershipPercentages[OwnershipPercent.Zero] = 0;
         OwnershipPercentages[OwnershipPercent.Ten] = 10;

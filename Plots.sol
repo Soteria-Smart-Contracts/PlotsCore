8464517010d936a0bc8016426b3301a9822446b6
Darius Tanz <scratchticketseb@gmail.com>
2023-12-30 21:27:10 +0000

    Dec 30, 2023, 4:27 PM

--- Plots.sol
+++ Plots.sol
@@ -774,7 +774,7 @@ contract NFTLoan{
     }
 
     function DisperseRewards(address RewardToken) public {
-        require(msg.sender == Owner || msg.sender == Borrower, "Only Owner or Borrower can disperse rewards");
+        require(msg.sender == Owner || msg.sender == Borrower || , "Only Owner or Borrower can disperse rewards");
         uint256 RewardBalance = ERC20(RewardToken).balanceOf(address(this));
         require(RewardBalance > 0, "No rewards to disperse");
         //check core contract for fee percentage and fee receiver, calculate fee and send to fee receiver

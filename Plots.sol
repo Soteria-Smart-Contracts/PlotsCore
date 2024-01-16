7a8398ddd3940cedd45d9efb653c704f4f13dd88
Darius Tanz <scratchticketseb@gmail.com>
2024-01-16 20:25:14 +0000

    Jan 16, 2024, 3:25 PM

--- Plots.sol
+++ Plots.sol
@@ -195,7 +195,6 @@ contract PlotsCoreV1 {
         require(NFTLoan(LoanContract).Active(), "Loan not active");
         require(NFTLoan(LoanContract).LoanEndTime() > block.timestamp, "Loan expired");
         OwnershipPercent CurrentOwnership = NFTLoan(LoanContract).OwnershipType();
-        require(OwnershipPercentages[Ownership] != 0, "Ownership not available");
         require(CurrentOwnership != Ownership, "Ownership already set to this");
 
         uint256 CurrentValue = PlotsTreasuryV1(Treasury).GetTokenValueFloorAdjusted(NFTLoan(LoanContract).TokenCollection(), NFTLoan(LoanContract).TokenID());

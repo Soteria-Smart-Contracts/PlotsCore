dd3f0c2f241d97c81d1bf06ab2d2bcfffb95a312
Darius Tanz <scratchticketseb@gmail.com>
2024-02-09 21:14:16 +0000

    Feb 9, 2024, 4:14 PM

--- Plots.sol
+++ Plots.sol
@@ -232,7 +232,7 @@ contract PlotsCoreV1 {
         ListedBool[Collection][TokenId] = true;
     }
 
-    //auto list function 
+    //auto list function for that can be called by treasury or lending contract
 
     function DelistToken(address Collection, uint256 TokenId) public{
         require(ListedCollectionsMap[Collection] == true && ListingsByCollection[Collection][ListingsByCollectionIndex[Collection][TokenId]].Lister != address(0), "Collection not listed or token not listed");

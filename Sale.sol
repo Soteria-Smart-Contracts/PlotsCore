5137f7f2a63c83c3b5f99900898729aeb711ee28
Darius Tanz <scratchticketseb@gmail.com>
2024-05-08 21:08:30 +0000

    May 8, 2024, 5:08 PM

--- Sale.sol
+++ Sale.sol
@@ -49,3 +49,20 @@ interface ERC20 {
   function totalSupply() external view returns (uint);
   function CheckMinter(address AddytoCheck) external view returns(uint);
 }
+
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}

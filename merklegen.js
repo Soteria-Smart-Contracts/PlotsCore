10839089a12c7675ee6c464b35c8cb98538025d9
Darius Tanz <scratchticketseb@gmail.com>
2024-05-09 22:31:20 +0000

    May 9, 2024, 6:31 PM

--- merklegen.js
+++ merklegen.js
@@ -1,1 +1,45 @@
 console.log('merklegen.js loaded');
+
+// 2. Collect list of wallet addresses from competition, raffle, etc.
+// Store list of addresses in some data sheeet (Google Sheets or Excel)
+let whitelistAddresses = [
+    "0X5B38DA6A701C568545DCFCB03FCB875F56BEDDC4",
+    "0X5A641E5FB72A2FD9137312E7694D42996D689D99",
+    "0XDCAB482177A592E424D1C8318A464FC922E8DE40",
+    "0X6E21D37E07A6F7E53C7ACE372CEC63D4AE4B6BD0",
+    "0X09BAAB19FC77C19898140DADD30C4685C597620B",
+    "0XCC4C29997177253376528C05D3DF91CF2D69061A",
+    "0xdD870fA1b7C4700F2BD7f44238821C26f7392148" // The address in remix
+  ];
+
+// 3. Create a new array of `leafNodes` by hashing all indexes of the `whitelistAddresses`
+// using `keccak256`. Then creates a Merkle Tree object using keccak256 as the algorithm.
+//
+// The leaves, merkleTree, and rootHas are all PRE-DETERMINED prior to whitelist claim
+const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+
+// 4. Get root hash of the `merkleeTree` in hexadecimal format (0x)
+// Print out the Entire Merkle Tree.
+const rootHash = merkleTree.getRoot();
+console.log('Whitelist Merkle Tree\n', merkleTree.toString());
+console.log("Root Hash: ", rootHash);
+
+// ***** ***** ***** ***** ***** ***** ***** ***** // 
+
+// CLIENT-SIDE: Use `msg.sender` address to query and API that returns the merkle proof
+// required to derive the root hash of the Merkle Tree
+
+// ✅ Positive verification of address
+const claimingAddress = leafNodes[6];
+// ❌ Change this address to get a `false` verification
+// const claimingAddress = keccak256("0X5B38DA6A701C568545DCFCB03FCB875F56BEDDD6");
+
+// `getHexProof` returns the neighbour leaf and all parent nodes hashes that will
+// be required to derive the Merkle Trees root hash.
+const hexProof = merkleTree.getHexProof(claimingAddress);
+console.log(hexProof);
+
+// ✅ - ❌: Verify is claiming address is in the merkle tree or not.
+// This would be implemented in your Solidity Smart Contract
+console.log(merkleTree.verify(hexProof, claimingAddress, rootHash));

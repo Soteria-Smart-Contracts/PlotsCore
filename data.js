9e92dcc743c73f8a10d49b0197a2e36cc8e30c59
Darius Tanz <scratchticketseb@gmail.com>
2024-05-10 19:44:40 +0000

    May 10, 2024, 3:44 PM

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 {
     "data"; {
       "fifteen_fdv_users"; {
-        "length": 3476,
-        "users": [
+        "length"; 3476,
+        "users"; [
           {
             "address": "0x04ab4c210f4d9814cd12a8804d249ec387f8ec61",
             "points": 1367789

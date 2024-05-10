bd28f5588f6777be2b3634d5c6d86a172220b83d
Darius Tanz <scratchticketseb@gmail.com>
2024-05-10 17:34:50 +0000

    May 10, 2024, 1:34 PM

--- merklegen.js
+++ merklegen.js
@@ -29,6 +29,7 @@ whitelistAddresses = whitelistAddresses.map((address) => {
   return address.address + address.points;
 }
 
+console.log(whitelistAddresses);
 
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
 console.log("Leaf Nodes: ", leafNodes);

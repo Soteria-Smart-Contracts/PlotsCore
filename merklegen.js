59fb7dc19706acf6c3255b3fe71b86ae7bdc07fe
Darius Tanz <scratchticketseb@gmail.com>
2024-05-10 18:05:38 +0000

    May 10, 2024, 2:05 PM

--- merklegen.js
+++ merklegen.js
@@ -32,6 +32,8 @@ whitelistAddresses = whitelistAddresses.map((address) => {
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
 const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
 
+//save 
+
 // Get root hash of the `merkleeTree` in hexadecimal format (0x)
 // Print out the Entire Merkle Tree.
 const rootHash = merkleTree.getRoot();

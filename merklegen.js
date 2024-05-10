5578a963cc5c6612644781ee9614758d9c3b05a6
Darius Tanz <scratchticketseb@gmail.com>
2024-05-10 17:35:14 +0000

    May 10, 2024, 1:35 PM

--- merklegen.js
+++ merklegen.js
@@ -31,30 +31,30 @@ whitelistAddresses = whitelistAddresses.map((address) => {
 
 console.log(whitelistAddresses);
 
-const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
-console.log("Leaf Nodes: ", leafNodes);
-const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+// const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+// console.log("Leaf Nodes: ", leafNodes);
+// const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
 
-// Get root hash of the `merkleeTree` in hexadecimal format (0x)
-// Print out the Entire Merkle Tree.
-const rootHash = merkleTree.getRoot();
-const rootHashBytes32 = '0x' + merkleTree.getRoot().toString('hex');
-console.log("Root Hash: ", rootHashBytes32);
+// // Get root hash of the `merkleeTree` in hexadecimal format (0x)
+// // Print out the Entire Merkle Tree.
+// const rootHash = merkleTree.getRoot();
+// const rootHashBytes32 = '0x' + merkleTree.getRoot().toString('hex');
+// console.log("Root Hash: ", rootHashBytes32);
 
-// ***** ***** ***** ***** ***** ***** ***** ***** // 
+// // ***** ***** ***** ***** ***** ***** ***** ***** // 
 
-// CLIENT-SIDE: Use `msg.sender` address to query and API that returns the merkle proof
-// required to derive the root hash of the Merkle Tree
+// // CLIENT-SIDE: Use `msg.sender` address to query and API that returns the merkle proof
+// // required to derive the root hash of the Merkle Tree
 
-// ✅ Positive verification of address
-const claimingAddress = leafNodes[6];
-// ❌ Change this address to get a `false` verification
-// const claimingAddress = keccak256("0X5B38DA6A701C568545DCFCB03FCB875F56BEDDD6");
+// // ✅ Positive verification of address
+// const claimingAddress = leafNodes[6];
+// // ❌ Change this address to get a `false` verification
+// // const claimingAddress = keccak256("0X5B38DA6A701C568545DCFCB03FCB875F56BEDDD6");
 
-// `getHexProof` returns the neighbour leaf and all parent nodes hashes that will
-// be required to derive the Merkle Trees root hash.
-const hexProof = merkleTree.getHexProof(claimingAddress);
+// // `getHexProof` returns the neighbour leaf and all parent nodes hashes that will
+// // be required to derive the Merkle Trees root hash.
+// const hexProof = merkleTree.getHexProof(claimingAddress);
 
-// ✅ - ❌: Verify is claiming address is in the merkle tree or not.
-// This would be implemented in your Solidity Smart Contract
-console.log(merkleTree.verify(hexProof, claimingAddress, rootHash));
+// // ✅ - ❌: Verify is claiming address is in the merkle tree or not.
+// // This would be implemented in your Solidity Smart Contract
+// console.log(merkleTree.verify(hexProof, claimingAddress, rootHash));

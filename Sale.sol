bef0e23f2d82920d817f6185e3ad277ce7df3767
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:51:55 +0000

    May 31, 2024, 3:51 PM

--- Sale.sol
+++ Sale.sol
@@ -363,7 +363,7 @@ library SafeERC20 {
         address from,
         address to,
         uint256 value
-    ) internal {
+    ) external {
         // Call the token contract's transferFrom function
         (bool success, bytes memory data) = token.call(
             abi.encodeWithSelector(

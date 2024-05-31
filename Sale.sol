d89534c3a1f021d23fb2e7c7c09f6678d7d55f8c
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 17:49:40 +0000

    May 31, 2024, 1:49 PM

--- Sale.sol
+++ Sale.sol
@@ -119,7 +119,7 @@ contract Plots_MultiToken_Presale {
         require(GetSaleStatus() != SalePhase.Over, "Sale is over");
         uint256 plotsToReceive = ConvertEthToPlots(msg.value);
         require(plotsToReceive > 0, "Invalid amount");
-        //re
+        //require that the 
 
         if (PhaseRequested == UserType.TwentyFiveFDV) {
             require(VerifyCredentials(Proof, keccak256(abi.encodePacked(msg.sender))), "Invalid credentials");

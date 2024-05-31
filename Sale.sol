2a4356aed2b38b08c51bddb3a401b736fbabb8a4
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:26:55 +0000

    May 31, 2024, 3:26 PM

--- Sale.sol
+++ Sale.sol
@@ -180,8 +180,10 @@ contract Plots_MultiToken_Presale {
         emit TokensPurchased(msg.sender, plotsToReceive, USDC);
     }
 
-    //function to call for first time users to set their allocation
-    
+    //function to call for first time users to set their allocation in usd which is
+    function SetAllocation(uint256 UserPoints) public {
+        AllocationUsed[msg.sender] = UserPoints;
+    }
 
     // Utility Functions
 

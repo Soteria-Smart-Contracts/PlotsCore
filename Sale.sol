4d454821b611c71847cb858640b982128f1d6123
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 17:25:25 +0000

    May 31, 2024, 1:25 PM

--- Sale.sol
+++ Sale.sol
@@ -133,7 +133,7 @@ contract Plots_MultiToken_Presale {
         emit TokensPurchased(msg.sender, plotsToReceive, USDT);
     }
 
-    function PurchaseWithUSDC(uint256 amount) public {
+    function PurchaseWithUSDC(uint256 amount, UserType PhaseRequested, uint256 UserPoints) public {
         require(GetSaleStatus() != SalePhase.Over, "Sale is over");
         uint256 plotsToReceive = ConvertStableToPlots(amount);
         require(plotsToReceive > 0, "Invalid amount");

b19972543903369507ed3e711a05c61ce806cad6
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:18:35 +0000

    May 31, 2024, 3:18 PM

--- Sale.sol
+++ Sale.sol
@@ -158,6 +158,9 @@ contract Plots_MultiToken_Presale {
         require(GetSaleStatus() != SalePhase.Over, "Sale is over");
         uint256 plotsToReceive = ConvertStableToPlots(amount, PhaseRequested);
         require(plotsToReceive > 0, "Invalid amount");
+        require(AllocationUsed[msg.sender] + UserPoints <= UserPoints, "Invalid allocation");
+
+        AllocationUsed[msg.sender] += UserPoints;
 
         if (PhaseRequested == UserType.FifteenFDV) {
             require(UserPoints / 2 >= amount, "Invalid amount");

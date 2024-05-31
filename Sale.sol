d925086707d926ed21efa2dce933baac563f7349
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 17:27:10 +0000

    May 31, 2024, 1:27 PM

--- Sale.sol
+++ Sale.sol
@@ -110,7 +110,7 @@ contract Plots_MultiToken_Presale {
     }
 
     // Purchase Functions
-    function PurchaseWithETH(UserType PhaseRequested, uint256 UserPoints) public payable {
+    function PurchaseWithETH(UserType PhaseRequested, uint256 UserPoints, bytes32 Us) public payable {
         require(GetSaleStatus() != SalePhase.Over, "Sale is over");
         uint256 plotsToReceive = ConvertEthToPlots(msg.value);
         require(plotsToReceive > 0, "Invalid amount");

5a1ede3e325a518c2efb6f41c741784b369590e0
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:14:17 +0000

    May 31, 2024, 3:14 PM

--- Sale.sol
+++ Sale.sol
@@ -182,7 +182,7 @@ contract Plots_MultiToken_Presale {
     }
 
     function ConvertStableToPlots(uint256 amountIn, UserType rate) public view returns (uint256) {
-        //if the rate is 25FDV, then the conversion rate 
+        //if the rate is 25FDV, then the price is phase two price, else it is phase two price 
         return amountIn / GetVLNDPrice();
     }
 

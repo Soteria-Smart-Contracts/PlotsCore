cd684e576c064259107e3117e7cc6484878ae8a2
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:30:06 +0000

    May 31, 2024, 3:30 PM

--- Sale.sol
+++ Sale.sol
@@ -110,7 +110,11 @@ contract Plots_MultiToken_Presale {
         require(Allocation[msg.sender] + UserPoints <= UserPoints, "Invalid allocation");
         require(TotalRaised + StableEquivalent <= PhaseOneCap, "Sale cap reached");
 
-        AllocationSet[msg.sender] 
+        //if allocation is not set, set it and set the allocation to true
+        if (!AllocationSet[msg.sender]) {
+            SetAllocationInUSD(StableEquivalent);
+            AllocationSet[msg.sender] = true;
+        }
 
         if (PhaseRequested == UserType.FifteenFDV) {
             require(UserPoints / 2 >= StableEquivalent, "Invalid amount");

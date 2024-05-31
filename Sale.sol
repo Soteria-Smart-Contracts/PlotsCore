012881f87ac5aebf12639fb3b429b3b6caa8e442
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:46:15 +0000

    May 31, 2024, 3:46 PM

--- Sale.sol
+++ Sale.sol
@@ -349,15 +349,15 @@ contract ERC20 {
     }
 
     function safeTransfer(address token, address to, uint value) internal {
-	// bytes4(keccak256(bytes('transfer(address,uint256)')));
-	(bool success, bytes memory data) = token.call(
-		abi.encodeWithSelector(0xa9059cbb, to, value)
-	);
-	require(
-		success && (data.length == 0 || abi.decode(data, (bool))),
-		"TransferHelper: TRANSFER_FAILED"
-	);
-}
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(0xa9059cbb, to, value)
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "TransferHelper: TRANSFER_FAILED"
+        );
+    }
 
     function safeTransferFrom(address token, address from, address to, uint value) internal {
 	// bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));

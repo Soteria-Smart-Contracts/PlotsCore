72c76f13c3e8618fc0cc01a98d6e312d62b81ef2
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:07:16 +0000

    May 31, 2024, 3:07 PM

--- Sale.sol
+++ Sale.sol
@@ -175,6 +175,9 @@ contract Plots_MultiToken_Presale {
     function ConvertEthToStable(uint256 amountIn) public view returns (uint256) {
         //AggregatorV3Interface priceFeed = AggregatorV3Interface(USDTPriceFeed);
         //(, int256 priceusdt, , , ) = priceFeed.latestRoundData();
+         uint256 amountInWei = amountIn * 10**18; // Convert ETH to wei
+        uint256 usdAmount = (amountInWei * 10e1) / priceusdt;
+        return usdAmount;
         uint256 priceusdt = 261650782927308;
         return amountIn / uint256(priceusdt);
     }

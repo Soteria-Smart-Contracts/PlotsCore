5a89e62ca6a1bbd1f27d8567bd1fe0e6f12829fb
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:53:31 +0000

    May 31, 2024, 3:53 PM

--- Sale.sol
+++ Sale.sol
@@ -127,7 +127,7 @@ contract Plots_MultiToken_Presale {
         }
         
         TotalRaised += msg.value;
-        ERC20(PLOTS).transfer(msg.sender, plotsToReceive);
+        ISafeERC20(PLOTS).safeTransfer(msg.sender, plotsToReceive);
 
         emit TokensPurchased(msg.sender, plotsToReceive, address(0));
     }

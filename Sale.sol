2aeb5dd18db4e4a1f3a79c69c5808636105875b0
Darius Tanz <scratchticketseb@gmail.com>
2024-05-31 19:17:44 +0000

    May 31, 2024, 3:17 PM

--- Sale.sol
+++ Sale.sol
@@ -105,6 +105,7 @@ contract Plots_MultiToken_Presale {
         require(GetSaleStatus() != SalePhase.Over, "Sale is over");
         uint256 plotsToReceive = ConvertEthToPlots(msg.value, PhaseRequested);
         require(plotsToReceive > 0, "Invalid amount");
+        //REQUIRE the allocation used by the user is less than the user points
 
         AllocationUsed[msg.sender] += UserPoints;
 

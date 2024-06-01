46216abdd3cefb4c3d8b513fc86d1d41ca1431da
Darius Tanz <scratchticketseb@gmail.com>
2024-06-01 13:27:23 +0000

    Jun 1, 2024, 9:27 AM

--- data2.js
+++ data2.js
@@ -5,4 +5,12 @@ data1 = [{
 ];
 
 data2 = [{
-    
+    "address": "0x04ab4c210f4d9814cd12a8804d249ec387f8ec61"
+  },
+  {
+    "address": "0x04ab4c210f4d9814cd12a8804d249ec387f8ec62"
+  },
+  {
+    "address": "0x04ab4c210f4d9814cd12a8804d249ec387f8ec63"
+  }
+];

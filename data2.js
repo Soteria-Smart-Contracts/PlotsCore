f3f60e7b37d9400bfb04b6b0bb2f81618c731deb
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-06-03 16:08:15 +0000

    Jun 3, 2024, 12:08 PM

--- data2.js
+++ data2.js
@@ -1,15 +1,99 @@
-data1 = [
+
+ChatGPT
+To add the addresses from the given list to both data1 and data2 arrays with points and without points respectively, you can do the following:
+
+javascript
+Copy code
+const newData1 = [
     {
         "address": "0xc932b3a342658A2d3dF79E4661f29DfF6D7e93Ce",
-        "points": 1000
+        "points": 5000
     },
     {
         "address": "0x19c128C3ca3E853f3a093D946ba1c9De4Ac7b04F",
         "points": 500
+    },
+    {
+        "address": "0xF997f77Fce4369B8fe4E0ee92c8a70F2769Ea6Be",
+        "points": 5000
+    },
+    {
+        "address": "0x028d867a09f1a7279e3Aa23073b1861A8C86892e",
+        "points": 5000
+    },
+    {
+        "address": "0x0c654B3C828ef5b8e31E0169983f0f0a4f6C3cF2",
+        "points": 5000
+    },
+    {
+        "address": "0x21058310A45656910913C0EB9CAeCa2ae8877AdA",
+        "points": 5000
+    },
+    {
+        "address": "0x2f3cD26eC5468fdF5Ef6D6645bF27F6c29743b1a",
+        "points": 5000
+    },
+    {
+        "address": "0xe69e122cdF88b57CC09bF44B8F62E99582b08E04",
+        "points": 5000
+    },
+    {
+        "address": "0x2Ef95BdEb9f5856A049FF247F4fD960C77417A4B",
+        "points": 5000
+    },
+    {
+        "address": "0x0E277baC49E101b0Cb44A0E5C4Bf235B1FE8347c",
+        "points": 5000
+    },
+    {
+        "address": "0x3353C5395D5E299B0C90746E43f27c4757518da7",
+        "points": 5000
+    },
+    {
+        "address": "0x673B3FC86eFC6D083F8D7640B08b669DdCAcE83f",
+        "points": 5000
+    },
+    {
+        "address": "0xD7C051b9940A81566b9b940d7b7DAf8455A3997b",
+        "points": 5000
     }
 ];
 
-data2 = [{
-    "address": "0x69420c1aCdDEBa55283362CE9dE2401EAe863c83"
-}
+const newData2 = [
+    {
+        "address": "0x69420c1aCdDEBa55283362CE9dE2401EAe863c83"
+    },
+    {
+        "address": "0xF997f77Fce4369B8fe4E0ee92c8a70F2769Ea6Be"
+    },
+    {
+        "address": "0x028d867a09f1a7279e3Aa23073b1861A8C86892e"
+    },
+    {
+        "address": "0x0c654B3C828ef5b8e31E0169983f0f0a4f6C3cF2"
+    },
+    {
+        "address": "0x21058310A45656910913C0EB9CAeCa2ae8877AdA"
+    },
+    {
+        "address": "0x2f3cD26eC5468fdF5Ef6D6645bF27F6c29743b1a"
+    },
+    {
+        "address": "0xe69e122cdF88b57CC09bF44B8F62E99582b08E04"
+    },
+    {
+        "address": "0x2Ef95BdEb9f5856A049FF247F4fD960C77417A4B"
+    },
+    {
+        "address": "0x0E277baC49E101b0Cb44A0E5C4Bf235B1FE8347c"
+    },
+    {
+        "address": "0x3353C5395D5E299B0C90746E43f27c4757518da7"
+    },
+    {
+        "address": "0x673B3FC86eFC6D083F8D7640B08b669DdCAcE83f"
+    },
+    {
+        "address": "0xD7C051b9940A81566b9b940d7b7DAf8455A3997b"
+    }
 ];

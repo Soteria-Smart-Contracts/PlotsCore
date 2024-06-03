2c974f0f357c8f96844cdb32ab0cd6780c8877ce
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-06-03 15:28:57 +0000

    Jun 3, 2024, 11:28 AM

--- merklegen.js
+++ merklegen.js
@@ -1,5 +1,34 @@
 console.log('merklegen.js loaded');
 
+
+          await ethereum.request({ method: 'eth_requestAccounts' });
+          window.web3 = await new Web3(ethereum);
+          await getID();
+          if (netID != 61){ //Change and fix
+              console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to Ethereum Classic."); 
+              alert("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network.");
+              return("Failed to connect")
+          }
+          accountarray = await web3.eth.getAccounts();
+          DAOcore = new window.web3.eth.Contract(window.CoreABI, CoreAddress);
+          DAOvoting = new window.web3.eth.Contract(window.VotingABI, VotingAddress);
+          CLDtoken = new window.web3.eth.Contract(window.CLDABI, CLDaddress);
+          account = accountarray[0];
+          console.log('Logged In')
+          LoggedIn = true;
+          await GetHENS();
+          await RemoveOverlay();
+
+          localStorage.setItem("ClassicDAOLogin", "true");
+          
+          return(true)
+      } 
+      else { 
+          alert("No ETHER Wallet available")
+      }
+  }
+}
+
 //convert the whitelistAddresses to an array of addresses with the points number appended to the address end
 let WhitelistAddresses = data2.map((address) => {
   return address.address;

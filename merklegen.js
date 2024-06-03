ac8b0a8ce59205c48cc669168dea612850e25e5e
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-06-03 15:38:40 +0000

    Jun 3, 2024, 11:38 AM

--- merklegen.js
+++ merklegen.js
@@ -19,15 +19,15 @@ async function loginWithEth(){
 
 function GenerateStuff(){
     //convert the whitelistAddresses to an array of addresses with the points number appended to the address end
-    const WhitelistAddresses = data2.map(address => web3.utils.encodePacked({value: address.address, type: 'address'}));
+    let WhitelistAddresses = data2.map(address => web3.utils.encodePacked({value: address.address, type: 'address'}));
 
-    const ClaimantAddresses = data1.map(address => web3.utils.encodePacked({value: address.address + address.points, type: 'string'}));
+    let ClaimantAddresses = data1.map(address => web3.utils.encodePacked({value: address.address + address.points, type: 'string'}));
 
     WhitelistAddresses = WhitelistAddresses.concat(ClaimantAddresses);
 
     console.log(WhitelistAddresses);
 
-    const leafNodesWhitelist = WhitelistAddresses.map(addr => keccak256(addr));
+    let leafNodesWhitelist = WhitelistAddresses.map(addr => keccak256(addr));
     let WhitelistMerkleTree = new MerkleTree(leafNodesWhitelist, keccak256, { sortPairs: true });
 
     // Get the Merkle Root of the Merkle Tree

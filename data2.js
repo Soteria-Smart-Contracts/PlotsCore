59175bf7ceb6ad8dd7baeb373f8652cd2ddb830a
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-06-04 22:03:01 +0000

    Jun 4, 2024, 6:03 PM

--- data2.js
+++ data2.js
@@ -18,55 +18,55 @@ const data1 = [
     },
     {
         "address": "0x0c654b3c828ef5b8e31e0169983f0f0a4f6c3cf2",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x21058310a45656910913c0eb9caeca2ae8877ada",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x2f3cd26ec5468fdf5ef6d6645bf27f6c29743b1a",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0xe69e122cdf88b57cc09bf44b8f62e99582b08e04",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x2ef95bdeb9f5856a049ff247f4fd960c77417a4b",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x0e277bac49e101b0cb44a0e5c4bf235b1fe8347c",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x3353c5395d5e299b0c90746e43f27c4757518da7",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x673b3fc86efc6d083f8d7640b08b669ddcace83f",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0xd7c051b9940a81566b9b940d7b7daf8455a3997b",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0xf84cab8d75e40ba2ae3b179d5eacc9b909583428",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x5b51080d8fefec2b5f11e7275944d9816411febe",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0x03a829acd19db9e38da3c1868aba18d3dfb4d075",
-        "points": 5000
+        "points": 100000
     },
     {
         "address": "0xc424FE2D213B9fc4BBEc242e835A1677664C02d7",
-        "points": 5000
+        "points": 100000
     }
 ];
 

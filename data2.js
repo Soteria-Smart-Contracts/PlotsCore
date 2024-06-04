68a01fd46b4b49535e01ec2fc33406e848aea874
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-06-04 22:07:05 +0000

    Jun 4, 2024, 6:07 PM

--- data2.js
+++ data2.js
@@ -2,59 +2,59 @@
 const data1 = [ 
     {
         "address": "0xc932b3a342658a2d3df79e4661f29dff6d7e93ce",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x19c128c3ca3e853f3a093d946ba1c9de4ac7b04f",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0xf997f77fce4369b8fe4e0ee92c8a70f2769ea6be",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x028d867a09f1a7279e3aa23073b1861a8c86892e",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x0c654b3c828ef5b8e31e0169983f0f0a4f6c3cf2",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x21058310a45656910913c0eb9caeca2ae8877ada",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x2f3cd26ec5468fdf5ef6d6645bf27f6c29743b1a",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0xe69e122cdf88b57cc09bf44b8f62e99582b08e04",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x2ef95bdeb9f5856a049ff247f4fd960c77417a4b",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x0e277bac49e101b0cb44a0e5c4bf235b1fe8347c",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x3353c5395d5e299b0c90746e43f27c4757518da7",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x673b3fc86efc6d083f8d7640b08b669ddcace83f",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0xd7c051b9940a81566b9b940d7b7daf8455a3997b",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0xf84cab8d75e40ba2ae3b179d5eacc9b909583428",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0x5b51080d8fefec2b5f11e7275944d9816411febe",
@@ -62,11 +62,11 @@ const data1 = [
     },
     {
         "address": "0x03a829acd19db9e38da3c1868aba18d3dfb4d075",
-        "points": 100000
+        "points": 1000000
     },
     {
         "address": "0xc424FE2D213B9fc4BBEc242e835A1677664C02d7",
-        "points": 100000
+        "points": 1000000
     }
 ];
 

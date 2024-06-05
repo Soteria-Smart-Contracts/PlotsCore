50d060122bd7266dbad41463b55644552a5ac8fd
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-06-05 21:42:49 +0000

    Jun 5, 2024, 5:42 PM

--- data.js
+++ data.js
@@ -11,6 +11,10 @@ data1 = [{
     "points": 381791
   },
   {
+    "address": "0x72703b554a7089f93ff1fc6cc6c0e623900a7b80",
+    "points": 381791
+  },
+  {
     "address": "0xb0cbf149ba9e7d0efb63235a08768a7b63f0652f",
     "points": 322092
   },

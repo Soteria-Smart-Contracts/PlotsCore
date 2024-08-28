9ae141af6c552343dff9f67a9e29b7b5ec25cd55
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-08-28 14:48:21 +0000

    Aug 28, 2024, 11:48 AM

--- Plots.sol
+++ Plots.sol
@@ -571,6 +571,7 @@ contract PlotsLend {
     function WithdrawToken(address Collection, uint256 TokenId) public {
         require(TokenDepositor[Collection][TokenId] == msg.sender, "Not owner of token");
         //require that the token is not in a loan
+        require(!PlotsCore(PlotsCoreContract).InLoanBool(Collection, TokenId), "Token in loan");
 
         PlotsCore(PlotsCoreContract).AutoDelist(Collection, TokenId);
 

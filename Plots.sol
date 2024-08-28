b759bc6dccf494b33b5d9ac6960a7d781ee11c46
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2024-08-28 14:43:18 +0000

    Aug 28, 2024, 11:43 AM

--- Plots.sol
+++ Plots.sol
@@ -418,7 +418,11 @@ contract PlotsTreasury {
         require(IERC721(Collection).ownerOf(TokenId) == address(this), "Not owner of token");
         IERC721(Collection).transferFrom(address(this), msg.sender, TokenId);
 
-        PlotsCore(PlotsCoreContract).AutoDelist(Collection, TokenId);
+        if (!PlotsCore(PlotsCoreContract).IsTokenInLoan(Collection, TokenId)) {
+            PlotsCore(PlotsCoreContract).RemoveListingFromCollection(Collection, TokenId);
+        } else {
+            PlotsCore(PlotsCoreContract).CloseLoan(Collection, TokenId);
+        }
 
         RemoveTokenFromCollection(Collection, TokenId);
     }
